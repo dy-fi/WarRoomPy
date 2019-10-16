37f9f1965b41b53b96874ac336ae51b63f095fbf
Dylan Finn <dylanfinn89@gmail.com>
2019-10-15 22:44:15 -0700

    skateboard data collection and presentation

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
+from bson import ObjectId
 import scrapper
 
 app = Flask(__name__)
 app.config.from_pyfile("config.cfg")
 app.config['MONGO_URI'] = "mongodb://localhost:27017/wrdb"
-db = PyMongo(app).db
-
+mongo = PyMongo(app)
+db = mongo.db
 
 @app.route("/", methods = ['GET'])
 def index():
@@ -16,12 +17,19 @@ def index():
 
 @app.route("/room", methods = ['POST', 'GET'])
 def make_room():
+
+    # gets the room form
+    if request.method == 'GET':
+        return render_template('room-form.html')
+
+    # post request makes a new room
     if request.method == 'POST':
+        # load form data
         title = request.form['title']
         name = request.form['name']
         url = request.form['url']
         path = request.form['path']
-
+        # Place struct
         Place = {
             'name': name,
             'url': url,
@@ -29,19 +37,16 @@ def make_room():
             'values': []
         }
 
-        place_id = db.rooms.insert_one(Place).inserted_id
+        place_id = db.places.insert_one(Place).inserted_id
         newRoomList = [place_id]
-
+        # Room struct
         Room = {
             'title': title,
             'places': newRoomList,
         }
-
+        # insert and view the new room
         room_id = db.rooms.insert_one(Room).inserted_id
         return redirect(url_for("show_room", room_id=room_id))
-
-    if request.method == 'GET':
-        return render_template('room-form.html')
 
 
 @app.route("/rooms", methods = ['GET'])
@@ -50,19 +55,44 @@ def show_rooms():
     return render_template('rooms.html', rooms=rooms)
 
 
-@app.route("/show_room")
+@app.route("/show_room/<room_id>", methods = ['GET'])
 def show_room(room_id):
-    room = db.rooms.find_one_or_404(room_id)
+    # find room or bust!
+    room = db.rooms.find_one({ "_id" : ObjectId(oid=str(room_id)) })
+    print(room)
+    # list of dictionaries after its populated in the proceeding for loop
     result = []
-
+    # iterate through places and scrape data
     for place_id in room['places']:
-        place = db.rooms.find_one({ "_id": place_id })
+        place = db.places.find_one({ "_id": ObjectId(oid=str(place_id)) })
         data = scrapper.ScrapeXpath(place['url'], place['path'])
+        print(data)
+        # response packaging
         d = { 
             'name': place['name'],
-            'data': data
+            'data': data,
+            'values': place['values']
         }
+
         result.append(d)
+
+        # push new val to mongo document
+        db.places.update(
+        {"_id": ObjectId(oid=str(place_id))},
+        {
+            "$push":
+            {"values": data}
+        })
+
+        # pop to keep the value lists at 10
+        if len(place['values']) > 9:
+            db.places.update(
+            {"_id": ObjectId(oid=str(place_id))},
+            {
+                "$pop":
+                {"values": 1}
+            })
+        
     
     return render_template("room.html", room=room, data=result)
     
--- scrapper.py
+++ scrapper.py
@@ -8,15 +8,19 @@ path = "//*[@id=\"main-Nowcard-92c6937d-b8c3-4240-b06c-9da9a8b0d22b\"]/div/div/s
 def ScrapeXpath(url, path):
     page = requests.get(url)
     tree = html.fromstring(page.content)
-    text = tree.xpath(path)
+    text = tree.xpath(path + "/text()")
+    print(text)
     return text
 
 # get multiple data points from multiple xpaths from the same url
+# used for optimization of requests to the same URL
 def ScrapeXpaths(url, paths):
     page = requests.get(url)
+    # html tree to navigate
     tree = html.fromstring(page.content)
     result = []
 
+    # grab all values
     for i in paths:
         result.append(tree.xpath(i))
     
